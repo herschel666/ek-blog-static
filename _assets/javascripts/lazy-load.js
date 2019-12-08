ce573cdb3dba003a95f85be6a82029b5d669ae0e
Emanuel Kluge <kraemerseele@gmx.de>
2019-12-08 13:39:41 +0100

    feat: simplify lazy-loading

--- _assets/javascripts/lazy-load.js
+++ _assets/javascripts/lazy-load.js
@@ -1,20 +1,17 @@
-
-'use strict';
-
-import { bind } from './vendor/eventie/eventie';
-import { subscribe } from './vendor/vanilla-pubsub/PubSub';
-
-let elems = [];
-
 const lazyLoad = () => {
+  const elems = [...document.querySelectorAll('noscript[data-src]')];
   let i = elems.length - 1;
   const winHeight = window.innerHeight || document.documentElement.clientHeight;
   const yOffset = window.pageYOffset;
-  while ( i >= 0 ) {
-    if ( elems[i].parentNode.offsetTop < (winHeight + yOffset) ) {
-      let elem = document.createElement('p');
-      elem.innerHTML = `<img src="${elems[i].getAttribute('data-src')}" ` +
-        `alt="${elems[i].getAttribute('data-alt')}">`;
+  while (i >= 0) {
+    if (elems[i].parentNode.offsetTop < (winHeight + yOffset)) {
+      const elem = Object.assign(
+        document.createElement('img'),
+        {
+          src: elems[i].getAttribute('data-src'),
+          alt: elems[i].getAttribute('data-alt'),
+        }
+      )
       elems[i].parentNode.replaceChild(elem, elems[i]);
       elems.splice(i, 1);
     }
@@ -23,19 +20,4 @@ const lazyLoad = () => {
   return lazyLoad;
 };
 
-const getElems = () => {
-  const allElems = document.getElementsByTagName('noscript');
-  const len = allElems.length;
-  let i = 0;
-  elems.length = 0; // Resetting the array
-  for ( ; i < len; i += 1 ) {
-    if ( allElems[i].getAttribute('data-src') ) {
-      elems.push(allElems[i]);
-    }
-  }
-  setTimeout(() => window.scrollTo(0, 1), 400);
-};
-
-getElems();
-bind(window, 'scroll', lazyLoad());
-subscribe('ek.ajaxify.inserted', getElems);
+window.addEventListener('scroll', lazyLoad());
