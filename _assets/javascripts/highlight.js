8ee7b7dea831c5fcfa2aaf277ce4276618a52b6f
Emanuel Kluge <kraemerseele@gmx.de>
2021-05-30 15:36:45 +0200

    fix: rename outdated syntax

--- _assets/javascripts/highlight.js
+++ _assets/javascripts/highlight.js
@@ -1,11 +1,11 @@
 import hljs from 'highlight.js';
-import htmlbars from 'highlight.js/lib/languages/htmlbars';
+import handlebars from 'highlight.js/lib/languages/handlebars';
 import php from 'highlight.js/lib/languages/php';
 import css from 'highlight.js/lib/languages/css';
 import javascript from 'highlight.js/lib/languages/javascript';
 import ini from 'highlight.js/lib/languages/ini';
 
-hljs.registerLanguage('htmlbars', htmlbars);
+hljs.registerLanguage('handlebars', handlebars);
 hljs.registerLanguage('php', php);
 hljs.registerLanguage('css', css);
 hljs.registerLanguage('javascript', javascript);
